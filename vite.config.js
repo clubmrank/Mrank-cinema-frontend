cf94c52302799e646689b955edc79116c17babab
AndileMasela <maselaandile@gmail.com>
2022-09-07 02:52:13 +0200

    app works

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/mrank-cinema/",
+  // base: "/mrank-cinema-frontend/",
   plugins: [react()],
 });
