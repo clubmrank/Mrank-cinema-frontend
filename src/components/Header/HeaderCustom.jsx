e09fda88f7431bb761381a520a4223def76e789f
AndileMasela <maselaandile@gmail.com>
2022-09-12 15:04:35 +0200

    back btn header

--- src/components/Header/HeaderCustom.jsx
+++ src/components/Header/HeaderCustom.jsx
@@ -6,12 +6,17 @@ import { IoIosArrowRoundBack } from "react-icons/io";
 
 import { Button, Modal } from "antd";
 import { AiOutlineClose } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
 
 const HeaderCustom = ({ showModal, calander, text }) => {
+  const navigate = useNavigate();
+  const goBack = () => {
+    navigate(-1);
+  };
   return (
     <header className={styles.seatHeader}>
       <div className={styles.backIcon}>
-        <IoIosArrowRoundBack size="50px" />
+        <IoIosArrowRoundBack onClick={goBack} size="50px" />
       </div>
       <div className={styles.middleText}>
         <h2>{text}</h2>
